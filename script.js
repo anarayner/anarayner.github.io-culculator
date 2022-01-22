346e0d2489effa7a2b5700ec427be733eb5e89e4
Victoria <95590803+anarayner@users.noreply.github.com>
2022-01-22 10:54:45 -0500

    style changes

---
+++ script.js
@@ -0,0 +1,11 @@
+let equalPressed = 0;
+
+// Refer all buttons exclude AC and DEL
+
+const button = document.querySelectorAll(".input__button");
+
+const AC = document.getElementById("clear");
+const DEL = document.getElementById("delete");
+
+const input = document.getElementById("input");
+const equal = document.getElementById("equal");
